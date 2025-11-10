4b268a08fc7bb0cb005c0d603d81f80f2c265926
Joplay(dot)XYZ <joplayyt@web.de>
2025-11-10 08:23:27 +0100

    Added multiple wide fullscreen pages

--- _assets/js/iframe.js
+++ _assets/js/iframe.js
@@ -58,7 +58,25 @@ document.addEventListener('fullscreenchange', () => {
         frame.classList.add('full');
         console.log('Fullscreen entered');
 
-        if (pageUrl === '/_pages/bigtowertinysquare2') {
+        const Urls [
+            '/_pages/peopleplayground',
+            '/_pages/bigtowertinysquare2',
+            '/_pages/bigneontowertinysquare',
+            '/_pages/badtimesimulator',
+            '/_pages/badgxme',
+            '/_pages/cookieclicker',
+            '/_pages/fivenightsatfreddys',
+            '/_pages/fivenightsatfreddys2',
+            '/_pages/fivenightsatfreddys3',
+            '/_pages/fivenightsatfreddys4',
+            '/_pages/fivenightsatfreddyspizzasimulator',
+            '/_pages/fivenightsatfreddyssisterlocation',
+            '/_pages/fivenightsatfreddysworld',
+            '/_pages/fivenightsatfreddysultimatecustomnight'
+            
+        ];
+            
+        if (Urls.includes(pageUrl) {
             frame.classList.add('full-wide');
         }
     } else {
